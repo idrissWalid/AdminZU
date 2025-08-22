6b83fe5e0b2c0ad291c7b8b67008da2e533b9121
idrissWalid <101337354+idrissWalid@users.noreply.github.com>
2025-08-22 07:15:10 +0000

    Update back.py

--- back.py
+++ back.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 app.secret_key = 'supersecretkey'
 
 # Stockage votes en mémoire
-votes = {"Candidat1": 0, "Candidat2": 0}
+votes = {"Candidat1": 0, "Candidat2": 0 , "Candidat3": 0,"Candidat4": 0 ,"Candidat5": 0}
 
 # --- Route user vote page ---
 @app.route('/')
 def index():
-    return "API de vote en ligne. Utilisez les routes /vote/<candidate> pour voter.", 200
+    return " Utilisez les routes /vote/<candidate> pour voter 🥲 maintenantsi tu sais pas le faire cest mort😂😂.", 200
 
 # --- Route pour voter ---
 @app.route('/vote/<candidate>', methods=['POST'])
@@ -45,15 +45,26 @@ def dashboard():
     total = votes["Candidat1"] + votes["Candidat2"]
     percent_c1 = round((votes["Candidat1"] / total * 100), 2) if total else 0
     percent_c2 = round((votes["Candidat2"] / total * 100), 2) if total else 0
+    percent_c3 = round((votes["Candidat3"] / total * 100), 2) if total else 0
+    percent_c4 = round((votes["Candidat4"] / total * 100), 2) if total else 0
+    percent_c5 = round((votes["Candidat5"] / total * 100), 2) if total else 0
+
 
     return render_template(
         'admin_dashboard.html',
         total_votes=total,
         votes_c1=votes["Candidat1"],
         votes_c2=votes["Candidat2"],
+        votes_c3=votes["Candidat3"],
+        votes_c4=votes["Candidat4"],
+        votes_c5=votes["Candidat5"],
         percent_c1=percent_c1,
-        percent_c2=percent_c2
+        percent_c2=percent_c2,
+        percent_c3=percent_c3,
+        percent_c4=percent_c4,
+        percent_c5=percent_c5
     )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
